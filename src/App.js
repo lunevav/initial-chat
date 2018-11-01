8366267cf657b94f85f9edba1036f05592328d01
Aleksandr Lunev <alexander.lunev@gmail.com>
2018-11-01 11:20:05 +0200

    random color v.2

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       });
     }
 
-    randomColor(str) { // java String#hashCode
+    randomColor(str) {
         let hash = 0;
         for (let i = 0; i < str.length; i++) {
             hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -55,7 +55,16 @@ class App extends Component {
         let c = (hash & 0x00FFFFFF)
             .toString(16)
             .toUpperCase();
-        return '#'+"00000".substring(0, 6 - c.length) + c;
+        let hex = '#' + "00000".substring(0, 6 - c.length) + c;
+        let rgba;
+        if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+            rgba = hex.substring(1).split('');
+            if (c.length == 3) {
+                rgba = [rgba[0], rgba[0], rgba[1], rgba[1], rgba[2], rgba[2]];
+            }
+            rgba = '0x' + rgba.join('');
+            return 'rgba(' + [(rgba >> 16) & 255, (rgba >> 8) & 255, rgba & 255].join(',') + ',0.1)';
+        }
     }
 
   render() {

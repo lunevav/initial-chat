1d2b69cffff6fd63cdbb08a191354aa239506f8d
Jury Kratser <kratser88@gmail.com>
2018-11-02 23:34:54 +0200

    correction of scrolling errors and other

--- src/App.js
+++ src/App.js
@@ -22,57 +22,57 @@ class App extends Component {
     componentDidMount() { this._input.focus(); }
 
     componentWillMount() { window.addEventListener("beforeunload", this.onCloseFromGoogleChromeHandler) }
-    componentWillUnmount() {window.removeEventListener("beforeunload", this.onCloseFromGoogleChromeHandler) }
+    componentWillUnmount() { window.removeEventListener("beforeunload", this.onCloseFromGoogleChromeHandler) }
     onCloseFromGoogleChromeHandler = () => {
-        const { addUserAndUpdate,  sendMessage } = this.props;
+        const { addUserAndUpdate, sendMessage } = this.props;
         addUserAndUpdate(false);
         sendMessage('Bye bye! ' + localStorage.getItem('userData'));
     }
 
-    onValue = (e) => this.setState({message: e.target.value});
+    onValue = (e) => this.setState({ message: e.target.value });
 
     sendMessage = (e) => {
-      e.preventDefault();
-      this.props.sendMessage(this.state.message);
-      this.setState({
-          message: ""
-      });
+        e.preventDefault();
+        this.props.sendMessage(this.state.message);
+        this.setState({
+            message: ""
+        });
     };
 
-  render() {
+    render() {
 
-    const userList = this.props.userList.length > 0 ? this.props.userList.map(item => (
-        <li style={{ color: item.status ? 'green' : 'red' }} key={item.id}>
-            {item.name}
-        </li>
+        const userList = this.props.userList.length > 0 ? this.props.userList.map(item => (
+            <li style={{ color: item.status ? 'green' : 'red' }} key={item.id}>
+                {item.name}
+            </li>
         )
-    ) : <div>User list loading...</div>
-    return (
-      <div className="App">
-          <div className="active-users-list">
-              <ul>
-                  {userList}
-              </ul>
-          </div>
-          <div  className="messsages-container">
-              <MessageList
-                messages={this.props.MESSAGES}
-              />
-              {/* TODO CREATE COMPONENT WITH HANDLERS*/}
-              <div className="input-container">
-                  <form onSubmit={this.sendMessage}>
-                      <input
-                          value={this.state.message}
-                          placeholder="Write a message..."
-                          ref={(ref) => this._input = ref}
-                          onChange={this.onValue} type="text"/>
-                      <button type="submit">SEND</button>
-                  </form>
-              </div>
-          </div>
-      </div>
-    );
-  }
+        ) : <div>User list loading...</div>
+        return (
+            <div className="App">
+                <div className="active-users-list">
+                    <ul>
+                        {userList}
+                    </ul>
+                </div>
+                <div className="messsages-container">
+                    <MessageList
+                        messages={this.props.MESSAGES}
+                    />
+                    {/* TODO CREATE COMPONENT WITH HANDLERS*/}
+                    <div className="input-container">
+                        <form onSubmit={this.sendMessage}>
+                            <input
+                                value={this.state.message}
+                                placeholder="Write a message..."
+                                ref={(ref) => this._input = ref}
+                                onChange={this.onValue} type="text" />
+                            <button type="submit">SEND</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {

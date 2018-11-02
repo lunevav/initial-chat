d4dceb8241a5580b48a1d465b09f87d356925be4
Aleksandr Lunev <alexander.lunev@gmail.com>
2018-11-02 17:45:30 +0200

    test

---
+++ src/utils/showNotification.js
@@ -0,0 +1,34 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+});
+
+ export function notifyMe(body) {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+
+        var notification = new Notification('Notification title', {
+            icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+            body: body,
+        });
+
+        notification.onclick = function () {
+            window.open("http://localhost:3000/");
+        };
+
+    }
+
+}
+
+
+export default  {
+    notifyMe
+}
